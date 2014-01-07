92c1afb6ac338abb1514ad448994db39b81b4a15
cup <bedreamer@163.com>
2014-01-06 21:07:11 -0800

    Add script to automatic generate a vs2005 project in current directory.

--- README.md
+++ README.md
@@ -9,4 +9,9 @@ project to vs2005 will make edit convenient.
 enjoy it!
 
 USAGE:
-$ keil2vs2005.sh keilprojectfile > vs2005projectfile
+
+You can convert a keil project to vs2005 project use command line below:
+    $ keil2vs2005.sh keilprojectfile > vs2005projectfile
+
+you can create a new vs2005 project in current directory use command line below also:    
+    $ newvs2005.sh > vs2005projectfile
---
+++ newvs2005.sh
@@ -0,0 +1,225 @@
+#!/bin/bash
+
+#echo "<FILES>" >> $2
+FNAME=$1
+SRC_H=$(/bin/find . -name  *.h | sed 's/\//\\/' )
+SRC_C=$(/bin/find . -name  *.c | sed 's/\//\\/' )
+VSNAME=${FNAME%".uvproj"}
+
+echo "<?xml version=\"1.0\" encoding=\"gb2312\"?>"
+echo "<VisualStudioProject"
+echo "	ProjectType=\"Visual C++\""
+echo "	Version=\"8.00\""
+echo "	Name=\"$VSNAME\""
+echo "	ProjectGUID=\"{624A370E-3D0F-411C-BEB7-025B73146EFD}\""
+echo "	RootNamespace=\"$VSNAME\""
+echo "	Keyword=\"Win32Proj\""
+echo "	>"
+tail --lines=161 $0 | sed 's/^$//'
+
+    echo "    <Files>"
+    echo "          <Filter"
+    echo "          Name=\"sources\""
+    echo "          Filter=\"c;c;cc;s\""
+    echo "          UniqueIdentifier=\"{4FC737F1-C7A5-4376-A066-2A32D752A2FF}\""
+    echo "          >"
+
+for f in $SRC_C; do
+    echo "        	<File"
+    echo "             	RelativePath=\""$f"\""
+    echo "             	>"
+    echo "        	</File>"
+done
+
+    echo "    	</Filter>"
+#:<<block
+    echo "    	<Filter"
+    echo "        	Name=\"herders\""
+    echo "        	Filter=\"h;hh;H;HH\""
+    echo "        	UniqueIdentifier=\"{93995380-89BD-4b04-88E9-625FBE52EBFB}\""
+    echo "          >"
+for f in $SRC_H; do
+    echo "          <File"
+    echo "             	RelativePath=\""$f"\""
+    echo "             	>"
+    echo "        	</File>"
+done
+    echo "    	</Filter>"
+#block
+
+echo "	</Files>"
+
+echo "	<Globals>"
+echo "	</Globals>"
+echo "</VisualStudioProject>"
+
+# .hH
+#cat $1 | grep '<FilePath>' | sed 's/<FilePath>//' | sed 's/<\/FilePath>//' | sed  's/ //g' | grep '\.[hH]' | sed 's/^/    <File \n        RelativePath=\"/' | sed 's/$/ ><\/FILE>/'
+#cat $1 | grep '^<FilePath^>' | sed 's/^<FilePath^>//' | sed 's/^<\/FilePath^>//' | grep ^.^[cCsS^]
+
+#echo "</FILES>" >> $2
+exit 0
+###########################################################
+# Data segment 
+###########################################################
+	<Platforms>
+		<Platform
+			Name="Win32"
+		/>
+	</Platforms>
+	<ToolFiles>
+	</ToolFiles>
+	<Configurations>
+		<Configuration
+			Name="Debug|Win32"
+			OutputDirectory="$(SolutionDir)$(ConfigurationName)"
+			IntermediateDirectory="$(ConfigurationName)"
+			ConfigurationType="1"
+			CharacterSet="1"
+			>
+			<Tool
+				Name="VCPreBuildEventTool"
+			/>
+			<Tool
+				Name="VCCustomBuildTool"
+			/>
+			<Tool
+				Name="VCXMLDataGeneratorTool"
+			/>
+			<Tool
+				Name="VCWebServiceProxyGeneratorTool"
+			/>
+			<Tool
+				Name="VCMIDLTool"
+			/>
+			<Tool
+				Name="VCCLCompilerTool"
+				Optimization="0"
+				PreprocessorDefinitions="WIN32;_DEBUG;_CONSOLE"
+				MinimalRebuild="true"
+				BasicRuntimeChecks="3"
+				RuntimeLibrary="3"
+				UsePrecompiledHeader="0"
+				WarningLevel="3"
+				Detect64BitPortabilityProblems="true"
+				DebugInformationFormat="4"
+			/>
+			<Tool
+				Name="VCManagedResourceCompilerTool"
+			/>
+			<Tool
+				Name="VCResourceCompilerTool"
+			/>
+			<Tool
+				Name="VCPreLinkEventTool"
+			/>
+			<Tool
+				Name="VCLinkerTool"
+				LinkIncremental="2"
+				GenerateDebugInformation="true"
+				SubSystem="1"
+				TargetMachine="1"
+			/>
+			<Tool
+				Name="VCALinkTool"
+			/>
+			<Tool
+				Name="VCManifestTool"
+			/>
+			<Tool
+				Name="VCXDCMakeTool"
+			/>
+			<Tool
+				Name="VCBscMakeTool"
+			/>
+			<Tool
+				Name="VCFxCopTool"
+			/>
+			<Tool
+				Name="VCAppVerifierTool"
+			/>
+			<Tool
+				Name="VCWebDeploymentTool"
+			/>
+			<Tool
+				Name="VCPostBuildEventTool"
+			/>
+		</Configuration>
+		<Configuration
+			Name="Release|Win32"
+			OutputDirectory="$(SolutionDir)$(ConfigurationName)"
+			IntermediateDirectory="$(ConfigurationName)"
+			ConfigurationType="1"
+			CharacterSet="1"
+			WholeProgramOptimization="1"
+			>
+			<Tool
+				Name="VCPreBuildEventTool"
+			/>
+			<Tool
+				Name="VCCustomBuildTool"
+			/>
+			<Tool
+				Name="VCXMLDataGeneratorTool"
+			/>
+			<Tool
+				Name="VCWebServiceProxyGeneratorTool"
+			/>
+			<Tool
+				Name="VCMIDLTool"
+			/>
+			<Tool
+				Name="VCCLCompilerTool"
+				PreprocessorDefinitions="WIN32;NDEBUG;_CONSOLE"
+				RuntimeLibrary="2"
+				UsePrecompiledHeader="0"
+				WarningLevel="3"
+				Detect64BitPortabilityProblems="true"
+				DebugInformationFormat="3"
+			/>
+			<Tool
+				Name="VCManagedResourceCompilerTool"
+			/>
+			<Tool
+				Name="VCResourceCompilerTool"
+			/>
+			<Tool
+				Name="VCPreLinkEventTool"
+			/>
+			<Tool
+				Name="VCLinkerTool"
+				LinkIncremental="1"
+				GenerateDebugInformation="true"
+				SubSystem="1"
+				OptimizeReferences="2"
+				EnableCOMDATFolding="2"
+				TargetMachine="1"
+			/>
+			<Tool
+				Name="VCALinkTool"
+			/>
+			<Tool
+				Name="VCManifestTool"
+			/>
+			<Tool
+				Name="VCXDCMakeTool"
+			/>
+			<Tool
+				Name="VCBscMakeTool"
+			/>
+			<Tool
+				Name="VCFxCopTool"
+			/>
+			<Tool
+				Name="VCAppVerifierTool"
+			/>
+			<Tool
+				Name="VCWebDeploymentTool"
+			/>
+			<Tool
+				Name="VCPostBuildEventTool"
+			/>
+		</Configuration>
+	</Configurations>
+	<References>
+	</References>
